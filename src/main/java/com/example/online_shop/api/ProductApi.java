55c76ad70ea89ca135558c2641ad081b0eb507a9
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-07-24 13:09:33 +0600

    add delete, update, create product

---
+++ src/main/java/com/example/online_shop/api/ProductApi.java
@@ -0,0 +1,34 @@
+package com.example.online_shop.api;
+
+import com.example.online_shop.dto.ProductRequest;
+import com.example.online_shop.dto.SimpleResponse;
+import com.example.online_shop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class ProductApi {
+
+    private final ProductService productService;
+
+    @PostAuthorize("hasAuthority('ADMIN')")
+    @PostMapping
+    SimpleResponse saveProduct(@RequestBody ProductRequest productRequest) {
+        return productService.save(productRequest);
+    }
+
+    @PostAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/{id}")
+    SimpleResponse deleteProduct(@PathVariable Long id) {
+        return productService.delete(id);
+    }
+
+    @PostAuthorize("hasAuthority('ADMIN')")
+    @PutMapping("/{id}")
+    SimpleResponse updateProduct(@RequestBody ProductRequest productRequest, @PathVariable Long id) {
+        return productService.update(productRequest, id);
+    }
+}
