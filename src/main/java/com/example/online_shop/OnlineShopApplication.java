0c9ff3f2bf8566f0bdd9c26623ab3342fbcfd2d0
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-08-01 17:09:48 +0600

    delete print method

--- src/main/java/com/example/online_shop/OnlineShopApplication.java
+++ src/main/java/com/example/online_shop/OnlineShopApplication.java
@@ -8,9 +8,5 @@ public class OnlineShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineShopApplication.class, args);
-        print("Hello");
-    }
-    static void print(String s) {
-        System.out.println(s);
     }
 }
